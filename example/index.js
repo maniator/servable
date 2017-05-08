3259c8e5e0570ed620860dac3cacff62a183f9d4
Neal Lubin <naftalilubin@gmail.com>
2017-05-08 10:40:29 -0400

    Implement count, concat and sum functionality

--- example/index.js
+++ example/index.js
@@ -1,37 +1,40 @@
 const { Observable } = Servable;
 
-// const countObservable$ = Observable.interval(1000, 1);
+const countObservable$ = Observable.interval(1000, 1);
+
+const countSubscription = countObservable$
+  .take(10)
+  .map((n) => n * 5)
+  .filter((n) => n > 10).concat(
+  countObservable$
+    .take(5)
+)
+  .subscribe({
+    next (number) {
+      console.log('NEXT NUMBER: ', number);
+    },
+
+    error (errors) {
+      console.warn('I HAVE ERRORS', errors)
+    },
+
+    complete () {
+      console.log('I AM COMPLETE');
+    }
+  });
+
+// // test event binding
+// const inputObservable$ = Observable.fromEvent('input', document.getElementById('myInput'), (event) => event.currentTarget.value);
+// const divReverse = document.getElementById('myTextReverse');
+// const div = document.getElementById('myText');
 //
-// const countSubscription = countObservable$
-//   .take(10)
-//   .map((n) => n * 5)
-//   .filter((n) => n > 10)
-//   .subscribe({
-//     next (number) {
-//       console.log('NEXT NUMBER: ', number);
-//     },
+// console.log(inputObservable$);
 //
-//     error (errors) {
-//       console.warn('I HAVE ERRORS', errors)
-//     },
+// inputObservable$
+//   .map((text) => text.split('').reverse().join(''))
+//   .do((text) => divReverse.textContent = text)
+//   .subscribe();
 //
-//     complete () {
-//       console.log('I AM COMPLETE');
-//     }
-//   });
-
-// test event binding
-const inputObservable$ = Observable.fromEvent('input', document.getElementById('myInput'), (event) => event.currentTarget.value);
-const divReverse = document.getElementById('myTextReverse');
-const div = document.getElementById('myText');
-
-console.log(inputObservable$);
-
-inputObservable$
-  .map((text) => text.split('').reverse().join(''))
-  .do((text) => divReverse.textContent = text)
-  .subscribe();
-
-inputObservable$
-  .do((text) => div.textContent = text)
-  .subscribe();
+// inputObservable$
+//   .do((text) => div.textContent = text)
+//   .subscribe();
---
+++ src/operators/count.js
@@ -0,0 +1,24 @@
+import { Observable } from '../Observable';
+import { filter } from './filter';
+
+export const count = function (source$, countCallback = (value) => true) {
+  return new Observable(function ({next, error, complete }) {
+    let count = 0;
+    const subscription = filter(source$, countCallback).subscribe({
+      next () {
+        count++;
+      },
+      error,
+      complete () {
+        next(count);
+        complete();
+      }
+    });
+    
+    return () => subscription.unsubscribe();
+  });
+};
+
+Observable.prototype.count = function () {
+  return count(this);
+};
