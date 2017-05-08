c453e2c5aa2adeca02afc7e6dcd2ebe3d445bf0c
Neal Lubin <naftalilubin@gmail.com>
2017-05-08 11:34:06 -0400

    Implement max, min, and average

---
+++ src/operators/average.js
@@ -0,0 +1,26 @@
+import { Observable } from '../Observable';
+
+export const average = function (source$) {
+  return new Observable(function ({ next, error, complete }) {
+    let count = 0;
+    let total = 0;
+    
+    const subscription = source$.subscribe({
+      next(value) {
+        count++;
+        total += value;
+      },
+      error,
+      complete () {
+        next(total / count);
+        complete();
+      }
+    });
+    
+    return () => subscription.unsubscribe();
+  });
+};
+
+Observable.prototype.average = function () {
+  return average(this);
+};
---
+++ src/operators/max.js
@@ -0,0 +1,25 @@
+import { Observable } from '../Observable';
+
+export const max = function (source$) {
+  return new Observable(function ({ next, error, complete }) {
+    let maxNumber;
+    const subscription = source$.subscribe({
+      next(value) {
+        if (!maxNumber || value > maxNumber) {
+          maxNumber = value;
+        }
+      },
+      error,
+      complete () {
+        next(maxNumber);
+        complete();
+      }
+    });
+    
+    return () => subscription.unsubscribe();
+  });
+};
+
+Observable.prototype.max = function () {
+  return max(this);
+};
