680e1f1e02b3302c583531eb703d809cc111079a
Neal Lubin <naftalilubin@gmail.com>
2017-05-10 09:59:07 -0400

    Reorganize src files into separate files

--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,7 @@
 export const noop = () => null;
 
+export * from './utilities/makeHot';
+
 export const isPromise = (p) => p !== null && typeof p === 'object' && typeof p.then === 'function';
 
 export const isObservable = (o$) => o$ !== null && typeof o$ === 'object' && typeof o$.subscribe === 'function';
