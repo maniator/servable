771012de0b21c75187259e575a817f9c0423b29f
Neal Lubin <naftalilubin@gmail.com>
2018-11-23 14:41:20 -0500

    Add some tests for the Observable class

--- src/Observable.test.js
+++ src/Observable.test.js
@@ -1,11 +1,65 @@
 import { Observable } from "./Observable";
+import { Subscription } from "./Subscription";
 
-test('new Observable will give you a new Observable instance', () => {
-  expect(new Observable()).toBeInstanceOf(Observable);
+describe('Observable', () => {
+  test('new Observable will give you a new Observable instance', () => {
+    expect(new Observable()).toBeInstanceOf(Observable);
+    expect(Observable.create()).toBeInstanceOf(Observable);
+  });
+
+  test('observerCallback is the same as the function passed to the constructor', () => {
+    const callback = () => null;
+    const obs$ = new Observable(callback);
+    expect(obs$.observerCallback).toBe(callback);
+  });
+
+  test('subscribe returns a subscription object', () => {
+    const obs$ = new Observable();
+    expect(obs$.subscribe()).toBeInstanceOf(Subscription);
+  });
+
+  test('the observerCallback will not be called when subscribe is never called', () => {
+    expect.assertions(0);
+    new Observable(() => {
+      expect(true).toBeTruthy();
+    });
+  });
+
+  test('the observerCallback will be called when subscribe is called once', () => {
+    expect.assertions(1);
+    const obs$ = new Observable(() => {
+      expect(true).toBeTruthy();
+    });
+    obs$.subscribe();
+  });
+
+  test('the error callback will get hit if observerCallback throws an error', () => {
+    expect.assertions(2);
+    const obs$ = new Observable(() => {
+      expect(true).toBeTruthy();
+
+      throw new Error('I have an error');
+    });
+    obs$.subscribe({
+      error() {
+        expect(true).toBeTruthy()
+      }
+    });
+  });
+
+  test('the error callback will receive the error thrown by the observerCallback', () => {
+    expect.assertions(2);
+    const error = new Error('I have an error');
+    const obs$ = new Observable(() => {
+      expect(true).toBeTruthy();
+
+      throw error;
+    });
+    obs$.subscribe({
+      error(_error) {
+        expect(_error).toBe(error)
+      }
+    });
+  });
 });
 
-test('observerCallback is the same as the function passed to the constructor', () => {
-  const callback = () => null;
-  const obs$ = new Observable(callback)
-  expect(obs$.observerCallback).toBe(callback);
-});
